c10cecdb44b39a572249e861093946674e4a92d0
Afonso Junior <ahsbjunior@gmail.com>
2016-06-11 13:54:36 -0300

    configurando as variáveis de ambiente

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_x8sddj6q:tory13@ds053638.mlab.com:53638/heroku_x8sddj6q',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'ggs-dev',
+  masterKey: process.env.MASTER_KEY || '03230aa03b9287a2f090ae92baeb7b22', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://ggs-dev.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
